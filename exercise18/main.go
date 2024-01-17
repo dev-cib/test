6ffaa2bec6407b9b682bda669fce1268943b4984
dev-cib <cib.udemy@gmail.com>
2024-01-17 11:49:13 -0600

    once again

--- exercise18/main.go
+++ exercise18/main.go
@@ -5,3 +5,8 @@ import "fmt"
 func main() {
 	fmt.Println("hello gophers")
 }
+
+// look in the $GOPATH/bin (go env)
+// "go install" your program/look at exe in $GOPATH/bin (go install .)
+// run exe
+// remove exe
